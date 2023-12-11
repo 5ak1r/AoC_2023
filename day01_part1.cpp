6904e44f4194f435c8f11c107e01ad3774eaeea4
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-12-11 17:02:05 +0000

    d1p1 done

---
+++ day01_part1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+
+    std::ifstream file("input_day01.txt");
+    
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    long grand_total = 0;
+    while(std::getline(file, line)) {
+        int n = line.length();
+        long total = 0;
+
+        for (int i=0; i < n; i++) {
+            if (isdigit(line[i])) {
+                total += (line[i] - '0')*10;
+                break;
+            } 
+        }
+
+        for (int i=n-1; i >= 0; i--) {
+            if (isdigit(line[i])) {
+                total += line[i] - '0';
+                break;
+            } 
+        }
+        grand_total += total;
+
+    }
+
+    std::cout << grand_total << '\n';
+    file.close();
+
+    return 0;
+}
