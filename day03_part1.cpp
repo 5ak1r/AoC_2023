4c35c6e80c7a4fc9a9e716ed983f3fe257c8425a
Sakir <sakir.azimkar@icloud.com>
2023-12-14 15:31:43 +0000

    computer broke, osk time

--- day03_part1.cpp
+++ day03_part1.cpp
@@ -3,9 +3,13 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <array>
 
-std::vector<int> index_num;
-std::vector<int> index_spc;
+std::vector<std::array<int, 3>> index_num;
+std::vector<std::array<int, 2>> index_spc;
+
+int first_index;
+int last_index;
 
 int main() {
     std::ifstream file("input_day03.txt");
@@ -16,7 +20,35 @@ int main() {
     }
 
     std::string line;
+    int counter = 1;
 	while(std::getline(file, line)) {
-		
+        for(int i = 0; i < line.length(); i++) {
+            if (isdigit(line[i])) {
+                first_index = i;
+                last_index = i+1;
+
+                while(isdigit(line[last_index])) {
+                    last_index++;
+                }
+                
+                i = last_index;
+                index_num.push_back({counter, first_index, last_index});
+
+            } else if (line[i] != '.') {
+                index_spc.push_back({counter, i});
+            }
+        }
+        counter++;
 	}
+
+    for(const auto& current_array : index_num) {
+        for(int current_num : current_array) {
+            std::cout << current_num << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+
+
 }
