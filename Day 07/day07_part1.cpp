ac5c2732f58ba10422397e22939842326a037df8
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-12-24 16:10:20 +0000

    d7p1 started

---
+++ Day 07/day07_part1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int calc_kind(std::string);
+
+//finally using structs sorry about previous solutions
+typedef struct {
+    std::string hand;
+    int bid;
+    int kind;
+} Cards;
+
+std::vector<Cards> cards;
+
+std::string hand;
+int bid;
+
+int calc_kind(std::string hand) {
+    std::map<char, int> dupes;
+
+    for(auto card: hand) {
+        dupes[card] += 1;
+    }
+
+    return std::max_element(dupes.begin(), dupes.end(),
+    [](const auto& val1, const auto& val2) { return val1.second < val2.second; })->second;
+}
+
+
+int main() {
+    std::ifstream file("input_day07.txt");
+        
+    if (!file.is_open()) {
+        perror(NULL);
+        return 1;
+    }
+
+    while(file >> hand) {
+        file >> bid;
+        cards.push_back(Cards{hand, bid, calc_kind(hand)});
+    }
+
+    for (auto card: cards) {
+        std::cout << card.bid << " " << card.hand << " " << card.kind << '\n';
+    }
+}
