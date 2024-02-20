f42340ad7fc96eafb8657466d288bf87692349ac
Sakir <sakir.azimkar@icloud.com>
2024-02-20 17:52:40 +0000

    d11p1 started

--- Day 11/day11_part1.cpp
+++ Day 11/day11_part1.cpp
@@ -2,19 +2,50 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <set>
 
 
-std::vector<std::string> find_galaxies(std::vector<std::string> image) {
-    
+
+std::vector<std::string> expansion(std::vector<std::string> space) {
+  int max_rows = space.size();
+  int max_cols = space[0].size();
+
+  std::set<int> planet_row;
+  std::set<int> planet_col;
+
+  for(int i = 0; i < max_rows; i++) {
+    for(int j = 0; j < max_cols; j++) {
+      if(space[i][j] == '#') {
+        planet_row.insert(i);
+        planet_col.insert(j);
+      }
+    }
+  }
+
+  
+  for(auto row: planet_row) {
+    std::cout << "row: " << row << std::endl;
+  }
+
+  for(auto col: planet_col) {
+    std::cout << "col: " << col << std::endl;
+  }
+
 }
 
 int main() {
   std::ifstream file("input_day11.txt");
+  std::vector<std::string> space;
 
   if (!file.is_open()) {
     perror(NULL);
     return 1;
   }
 
+  for (std::string line; std::getline(file, line);) {
+    space.push_back(line);
+  }
+
+  space = expansion(space);
 
 }
