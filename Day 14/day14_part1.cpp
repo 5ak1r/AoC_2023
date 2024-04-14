9579f1346fa715202166b9f93cc34c6ac73a6449
5ak1r <s.azimkar1@newcastle.ac.uk>
2024-04-14 21:27:41 +0100

    d14p1 done, working on d14p2

--- Day 14/day14_part1.cpp
+++ Day 14/day14_part1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 
 int main() {
@@ -27,7 +28,48 @@ int main() {
     }
   }
   
+  size_t cube_rock_index;
+  size_t smooth_index;
+  size_t next_free;
+
+  size_t total = 0;
+
   for(auto c: cols) {
-    std::cout << c << std::endl;
+    next_free = 0;
+    cube_rock_index = c.find('#');
+    smooth_index = c.find('O');
+
+    while(smooth_index != std::string::npos) {
+      smooth_index = c.find('O');
+
+      while(smooth_index < cube_rock_index) {
+        c[next_free] = 'S';
+
+        if(next_free != smooth_index) {
+          c[smooth_index] = '.';
+        }
+
+        smooth_index = c.find('O');
+        next_free++;
+      }
+
+      if(cube_rock_index != std::string::npos) {
+        next_free = cube_rock_index + 1;
+        c[cube_rock_index] = 'R';
+        cube_rock_index = c.find('#');
+      }
+      
+    }
+
+    std::replace(c.begin(), c.end(), 'R', '#');
+
+    for(int i = 0; i < c.size(); i++) {
+      if(c[i] == 'S') {
+        total += c.size() - i;
+      }
+    }
+
   }
+
+  std::cout << total << std::endl;
 }
