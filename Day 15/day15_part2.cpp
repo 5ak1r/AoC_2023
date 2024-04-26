3539ade2e20ca493dd4671b02ba43e25eb1fc95a
5ak1r <sakir.azimkar@icloud.com>
2024-04-26 12:27:51 +0100

    d15p2 done, lambda functions are cool :)

--- Day 15/day15_part2.cpp
+++ Day 15/day15_part2.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+
+int hash(std::string label) {
+    int current;
+
+    for(auto l: label) {
+        current += int(l);
+        current *= 17;
+        current %= 256;
+    }
+
+    return current;
+}
+
+std::map<int, std::vector<std::pair<std::string, int>>> boxes;
 
 int main() {
     std::ifstream file("input_day15.txt");
@@ -24,17 +41,55 @@ int main() {
     }
     pieces.push_back(line);
 
-    size_t current = 0;
-    size_t total = 0;
+
+    for(int i = 0; i < 256; i++) {
+        boxes[i] = {};
+    }
+
+    int current_box;
 
     for(auto p: pieces) {
-        for(auto l: p) {
-            current += int(l);
-            current *= 17;
-            current %= 256;
+        if(p.find('=') != std::string::npos) {
+            std::string label = p.substr(0, p.find('='));
+            current_box = hash(label);
+            int focal = std::stoi(p.substr(p.find('=') + 1, p.size()));
+
+            auto it = std::find_if(boxes[current_box].begin(), boxes[current_box].end(), [label](const auto& pair) {
+                return pair.first == label;
+            });
+
+            if(it != boxes[current_box].end()) {
+                it->second = focal;
+            } else {
+                boxes[current_box].push_back(std::make_pair(label, focal));
+            }
+
+        } else {
+            std::string label = p.substr(0, p.size() - 1);
+            current_box = hash(label);
+
+            auto it = std::find_if(boxes[current_box].begin(), boxes[current_box].end(), [label](const auto& pair) {
+                return pair.first == label;
+            });
+
+            if(it != boxes[current_box].end()) {
+                boxes[current_box].erase(it);
+            }
         }
-        total += current;
-        current = 0;
+    }
+
+    int total = 0;
+    int current = 0;
+    
+    for(auto it = boxes.begin(); it != boxes.end(); it++) {
+        int counter = 1;
+        for(auto itt: it->second) {
+
+            current = (it->first + 1) * counter * itt.second;
+            total += current;
+            //std::cout << current << std::endl;
+            counter++;
+        }
     }
 
     std::cout << total << std::endl;
