f65058fb264579294c82cadc571a1d7ce6a1a881
5ak1r <s.azimkar1@newcastle.ac.uk>
2024-07-08 19:12:53 +0100

    need to work on dijkstras

--- Day 17/day17_part1.cpp
+++ Day 17/day17_part1.cpp
@@ -8,19 +8,6 @@
 #define graph std::vector<std::vector<int>>
 
 void dijkstra(graph weights) {
-    graph dist;
-    std::vector<int> visited;
-    
-    for(int i = 0; i < weights.size(); i++) {
-        for(int j = 0; j < weights[0].size(); j++) {
-            dist[i][j] = 999999;
-            visited.push_back(0);
-        }
-    }
-    dist[0][0] = 0;
-
-    while(std::find(visited.begin(), visited.end(), 0) != visited.end()) {
-    }
 }
 
 int main() {
@@ -37,11 +24,12 @@ int main() {
     int count = 0;
 
     while(std::getline(file, line)) {
+
+        weights.push_back({});
+
         for(char i: line) {
             weights[count].push_back(i - '0');
-            std::cout << weights[count][0];
         }
-
         count++;
     }
 
